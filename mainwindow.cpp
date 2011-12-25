9c1d1a7ada01d145e25f06d9c42baae891a45b61
Alex Sychev <alex@sychev.com>
2011-12-25 12:17:35 +0400

    ShowNormal method was replaced to show().

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -58,7 +58,7 @@ void MainWindow::integrateWithAppModel()
 
 void MainWindow::showWindow()
 {
-    showNormal();
+    show();
     activateWindow();
     setWindowState(windowState() | Qt::WindowFullScreen);
     setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
