274f625524a4744bb6150d138521ce9cdd4057a1
Alex Sychev <194145@gmail.com>
2012-07-07 20:06:02 +0400

    Packaging commands were moved to one file.

--- packaging/windows/build-windows.bat
+++ packaging/windows/build-windows.bat
@@ -1,2 +1,19 @@
-cmd /C "packaging\windows\build.bat -clean"
-cmd /C packaging\windows\installer.bat
+rem Make sure these paths are appropriate for your system:
+path = %PATH%;C:\QtSDK\Desktop\Qt\4.8.1\mingw\bin;C:\QtSDK\mingw\bin;
+qmake catlooking.pro
+mingw32-make release
+rem Make sure these paths are appropriate for your system:
+SET mingw_dir=C:\QtSDK\mingw
+SET qt_dir=C:\QtSDK\Desktop\Qt\4.8.1\mingw
+
+IF NOT EXIST build\windows MKDIR build\windows
+COPY %mingw_dir%\bin\mingwm10.dll build\windows
+COPY %mingw_dir%\bin\libgcc_s_dw2-1.dll build\windows
+COPY %qt_dir%\bin\QtCore4.dll build\windows
+COPY %qt_dir%\bin\QtGui4.dll build\windows
+COPY %qt_dir%\bin\QtXml4.dll build\windows
+COPY %qt_dir%\bin\QtNetwork4.dll build\windows
+IF NOT EXIST build\windows\imageformats MKDIR build\windows\imageformats
+COPY %qt_dir%\plugins\imageformats\qjpeg4.dll build\windows\imageformats
+"C:\Program Files (x86)\NSIS\Unicode\makensis" packaging\windows\catlooking.nsi
+MOVE InstallCatlooking.exe build\windows\InstallCatlooking.exe
